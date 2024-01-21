bf73fa010fb49d0a7de46b6bdcd5535654d46168
MrKris7100 <czacha55@vp.pl>
2024-01-21 13:56:17 +0100

    Changed boost version to 1.73.0

--- external-libs/script/boost-fetch.sh
+++ external-libs/script/boost-fetch.sh
@@ -6,8 +6,8 @@ source script/env.sh
 
 cd $EXTERNAL_LIBS_BUILD_ROOT
 
-version=1_58_0
-dot_version=1.58.0
+version=1_73_0
+dot_version=1.73.0
 
 if [ ! -f "boost_${version}.tar.gz" ]; then
   wget https://downloads.sourceforge.net/project/boost/boost/${dot_version}/boost_${version}.tar.gz
